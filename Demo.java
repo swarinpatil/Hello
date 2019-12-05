6bb80e2254ea387d12a3c897a4e78fe8c93f6e5b
swarinpatil <58510187+swarinpatil@users.noreply.github.com>
2019-12-05 11:31:17 +0530

    Update Demo.java
    This is updated

--- Demo.java
+++ Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is Demo");
+		System.out.println("this is Demo1");
+		
 	}
 
 
